c063e7992b4f972f2132dd856a5c2335b0ba015c
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-30 17:37:52 +0100

    Give up calling tmhmm directly

--- R/run_tmhmm.R
+++ R/run_tmhmm.R
@@ -4,7 +4,32 @@
 #' @author Richel J.C. Bilderbeek
 #' @export
 run_tmhmm <- function(
-  fasta_filename
+  fasta_filename,
+  folder_name = get_default_tmhmm_folder() # nolint tmhmm function
 ) {
-  data.frame(x = fasta_filename)
+  bin_path <- file.path(folder_name, "tmhmm-2.0c", "bin", "tmhmm")
+  old_path <- Sys.getenv("PATH")
+  path <- paste0(
+    paste0(file.path(folder_name, "tmhmm-2.0c"), ":"),
+    old_path
+  )
+  path <- paste0(
+    paste0(file.path(folder_name, "tmhmm-2.0c", "bin"), ":"),
+    path
+  )
+  path <- paste0(
+    paste0(file.path(folder_name, "tmhmm-2.0c", "lib"), ":"),
+    path
+  )
+  Sys.setenv(PATH = path)
+  testit::assert(path = Sys.getenv("PATH"))
+
+  text <- system2(
+    command = bin_path,
+    args = fasta_filename,
+    stdout = TRUE
+  )
+  data.frame(lines = text)
+  Sys.setenv(PATH = old_path)
+  testit::assert(old_path = Sys.getenv("PATH"))
 }
--- tests/testthat/test-run_tmhmm.R
+++ tests/testthat/test-run_tmhmm.R
@@ -1,7 +1,7 @@
 context("test-run_tmhmm")
 
 test_that("use", {
-  fasta_file <- system.file("extdata", "tmhmm.fasta", package = "tmhmm")
-  df <- run_tmhmm(fasta_file)
+  fasta_filename <- system.file("extdata", "tmhmm.fasta", package = "tmhmm")
+  df <- run_tmhmm(fasta_filename)
   expect_true(is.data.frame(df))
 })
