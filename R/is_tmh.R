2665c78956e80d23fa5c9da679dd083939589fe3
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-29 18:02:48 +0200

    EOD

--- R/is_tmh.R
+++ R/is_tmh.R
@@ -5,6 +5,7 @@
 #'
 #' expect_true(is_tmh("VVIILTIAGNILVIMAVSLE"))
 #' expect_false(is_tmh("VVIILTIRGNILVIMAVSLE"))
+#' @export
 is_tmh <- function(protein_sequence) {
   locatome <- tmhmm::run_tmhmm_on_sequence(protein_sequence)
   stringr::str_count(string = locatome, pattern = "(M|m)") > 0
