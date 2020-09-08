0b5f38740769cb9eae9258f2b973ca39328d0aa1
richelbilderbeek <richel@richelbilderbeek.nl>
2020-09-08 11:34:25 +0200

    Update doc, URL, vignette

--- R/get_tmhmm_url.R
+++ R/get_tmhmm_url.R
@@ -1,7 +1,9 @@
 #' Get an URL to download TMHMM from.
 #' These URLs expire.
 #' @author Richèl J.C. Bilderbeek
+#' @examples
+#' get_tmhmm_url()
 #' @export
 get_tmhmm_url <- function() {
-  "https://services.healthtech.dtu.dk/download/28c408dc-ef5e-47ad-a284-66754bcd27f7" # nolint indeed a long URL
+  "https://services.healthtech.dtu.dk/download/9b7393ac-5d97-40ae-a2cf-ccc99e7db910" # nolint indeed a long URL
 }
