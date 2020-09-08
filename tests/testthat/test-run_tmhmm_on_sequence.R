6f6f89d68a9cde30c9c69ebf126a1c61f265e4ca
richelbilderbeek <richel@richelbilderbeek.nl>
2020-09-08 14:06:20 +0200

    Expose #2

--- tests/testthat/test-run_tmhmm_on_sequence.R
+++ tests/testthat/test-run_tmhmm_on_sequence.R
@@ -9,14 +9,14 @@ test_that("use", {
   )
 })
 
-test_that("abuse", {
+test_that("Give proper error when a ? is in the protein sequence", {
   if (!is_tmhmm_installed()) return()
-
+  skip("Issue #2")
   # Escalated from https://github.com/richelbilderbeek/bbbq_article/issues/74
   protein_sequence <- "IMPRESSIVELYFLI?AWAYFANSWALKSWEETMARKETTRIVIALLYNAILIDENTIFY" # nolint indeed long
-  locatome <- run_tmhmm_on_sequence(protein_sequence)
-  expect_equal(
-    locatome,
-    "iiiiiiMMMMMMMMMMMMMMMMMMMMMMMooooooooooooooMMMMMMMMMMMMMMMMMMMMMMMiiiii" # nolint indeed long
+  expect_error(
+    run_tmhmm_on_sequence(protein_sequence),
+    "Character '?' not allowed in alphabet 'ACDEFGHIKLMNPQRSTVWYBXZ'."
   )
+
 })
