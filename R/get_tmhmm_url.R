b8427d483bcdb27c90c4b37fdb8fa4960b35e1c9
richelbilderbeek <richel@richelbilderbeek.nl>
2020-09-16 12:39:15 +0200

    Fresh URL

--- R/get_tmhmm_url.R
+++ R/get_tmhmm_url.R
@@ -5,5 +5,5 @@
 #' get_tmhmm_url()
 #' @export
 get_tmhmm_url <- function() {
-  "https://services.healthtech.dtu.dk/download/c1583ab4-48a1-4856-9d56-aff23aedf06e" # nolint indeed a long URL
+  "https://services.healthtech.dtu.dk/download/2d5f625a-b343-42a7-82b5-f7b20e141f13" # nolint indeed a long URL
 }
