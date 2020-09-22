7f7ca21db627b20a3c7ac9f72fd83e8215c55f2d
richelbilderbeek <richel@richelbilderbeek.nl>
2020-09-22 12:21:12 +0200

    Add 'check_topology'

---
+++ R/check_topology.R
@@ -0,0 +1,23 @@
+#' Check if the topology is valid.
+#'
+#' Check if the argument is of the same type as a predicted topology,
+#' as can be created with \link{predict_topology}.
+#' Will \link{stop} if not.
+#' @inheritParams default_params_doc
+#' @return Nothing. Will \link{stop} with a helpful error message if
+#' the topology is invalid.
+#' @author Richèl J.C. Bilderbeek
+#' @export
+check_topology <- function(topology) {
+  if (!tibble::is_tibble(topology)) {
+    stop("'topology' must be a tibble")
+  }
+  expected_names <- c("name", "topology")
+  if (length(names(topology)) != length(expected_names)) {
+    stop("'topology' must have the correct number of columns")
+  }
+
+  if (!all(names(topology) %in% expected_names)) {
+    stop("'topology' must have columns 'name' and 'topology'")
+  }
+}
---
+++ tests/testthat/test-check_topology.R
@@ -0,0 +1,28 @@
+test_that("use", {
+  expect_error(check_topology("nonsense"))
+  expect_error(check_topology(""))
+  expect_error(check_topology(NA))
+  expect_error(check_topology(NULL))
+  expect_error(check_topology(Inf))
+  expect_error(check_topology(3.14))
+  expect_error(
+    check_topology(
+      topology = tibble::tibble()
+    )
+  )
+  expect_error(
+    check_topology(
+      topology = tibble::tibble(name = "X", nonsense = "Y")
+    )
+  )
+
+})
+
+test_that("use", {
+  if (!is_pureseqtm_installed()) return()
+  fasta_filename <- system.file(
+    "extdata", "example_2.fasta", package = "tmhmm"
+  )
+  topology <- predict_topology(fasta_filename)
+  expect_silent(check_topology(topology))
+})
