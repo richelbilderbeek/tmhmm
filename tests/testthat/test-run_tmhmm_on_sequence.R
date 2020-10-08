c353ee7d0a1f4263dbb73fdec9ff27b8e206126d
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-08 10:24:03 +0200

    Expose #3

--- tests/testthat/test-run_tmhmm_on_sequence.R
+++ tests/testthat/test-run_tmhmm_on_sequence.R
@@ -9,6 +9,19 @@ test_that("use", {
   )
 })
 
+test_that("long sequence", {
+  if (!is_tmhmm_installed()) return()
+
+  skip("Issue #3")
+  # https://github.com/richelbilderbeek/tmhmm/issues/3
+  protein_sequence <- paste0(rep("FAMILYVW", 20), collapse = "")
+  topology <- run_tmhmm_on_sequence(protein_sequence)
+  expect_equal(
+    stringr::str_length(protein_sequence),
+    stringr::str_length(topology)
+  )
+})
+
 test_that("Give proper error when a ? is in the protein sequence", {
   if (!is_tmhmm_installed()) return()
 
