31e5c80c4ea02f4c9b39ee63b2f36fe711476a41
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-08 11:04:20 +0200

    'run_tmhmm_on_sequence' tests its input better

--- R/run_tmhmm_on_sequence.R
+++ R/run_tmhmm_on_sequence.R
@@ -1,22 +1,44 @@
 #' Run TMHMM directy on a protein sequence
+#'
+#' Run TMHMM directy on one protein sequence
 #' @inheritParams default_params_doc
 #' @param protein_sequence a protein sequence, with
 #'   the amino acids as capitals, for
 #'   example `MEILCEDNTSLSSIPNSL`
-#' @return the locatome
+#' @return the topology. The topology is a character string
+#'   with the same length as the
+#'   protein sequence. The topology consists of the characters
+#'   \code{i} ('inside'), \code{I} ('inside'),
+#'   \code{m} ('membrane'), \code{M} ('membrane'),
+#'   \code{o} ('outside') and \code{O} ('outside')
 #' @examples
 #' if (is_tmhmm_intalled()) {
 #'   protein_sequence <- paste0(
 #'     "QEKNWSALLTAVVIILTIAGNILVIMAVSLEKKLQNATNYFLM",
 #'     "SLAIADMLLGFLVMPVSMLTILYGYRWP"
 #'   )
-#'   locatome <- run_tmhmm_on_sequence(protein_sequence)
+#'   run_tmhmm_on_sequence(protein_sequence)
 #' }
 #' @export
 run_tmhmm_on_sequence <- function(
   protein_sequence,
   folder_name = get_default_tmhmm_folder()
 ) {
+  if (length(protein_sequence) != 1) {
+    stop(
+      "'protein_sequence' must be one sequence. \n",
+      "Actual length: ", length(protein_sequence)
+    )
+  }
+  if (!is.character(protein_sequence)) {
+    stop(
+      "'protein_sequence' must be one character sequence. \n",
+      "Actual type: ", class(protein_sequence)
+    )
+  }
+  if (nchar(protein_sequence) == 0) {
+    stop("'protein_sequence' must have at least one character")
+  }
   tmhmm::check_tmhmm_installation()
 
   fasta_filename <- tempfile()
--- tests/testthat/test-run_tmhmm_on_sequence.R
+++ tests/testthat/test-run_tmhmm_on_sequence.R
@@ -22,12 +22,29 @@ test_that("long sequence", {
   )
 })
 
+test_that("abuse without TMHMM installed", {
+  expect_error(
+    run_tmhmm_on_sequence(protein_sequence = NA),
+    "'protein_sequence' must be one character sequence"
+  )
+  expect_error(
+    run_tmhmm_on_sequence(protein_sequence = NULL),
+    "'protein_sequence' must be one sequence"
+  )
+  expect_error(
+    run_tmhmm_on_sequence(protein_sequence = ""),
+    "'protein_sequence' must have at least one character"
+  )
+  expect_error(
+    run_tmhmm_on_sequence(protein_sequence = c("FAMILYVW", "FAMILYVW")),
+    "'protein_sequence' must be one sequence"
+  )
+})
+
 test_that("Give proper error when a ? is in the protein sequence", {
   if (!is_tmhmm_installed()) return()
-
-  protein_sequence <- "IMPRESSIVELYFLI?AWAYFANSWALKSWEETMARKETTRIVIALLYNAILIDENTIFY" # nolint indeed long
   expect_error(
-    run_tmhmm_on_sequence(protein_sequence),
+    run_tmhmm_on_sequence(protein_sequence = "?"),
     "Character '\\?' not allowed in alphabet 'ACDEFGHIKLMNPQRSTVWYBXZ'."
   )
 })
