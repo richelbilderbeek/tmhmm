823e4ff2d00b182e8078ccd989be7ccb891b3e1b
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-08 11:13:30 +0200

    Fresh URL

--- R/get_tmhmm_url.R
+++ R/get_tmhmm_url.R
@@ -5,5 +5,5 @@
 #' get_tmhmm_url()
 #' @export
 get_tmhmm_url <- function() {
-  "https://services.healthtech.dtu.dk/download/30564f13-1b7f-48f6-82ab-7ff3c9252610" # nolint indeed a long URL
+  "https://services.healthtech.dtu.dk/download/98a1be17-577e-4cb0-8f65-f787a4591064" # nolint indeed a long URL
 }
